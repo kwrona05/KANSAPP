abfaa9571cc08ec933e5a4b354ff52a6ec459fa8
kwrona05 <kwrona05@gmail.com>
2025-02-08 12:55:32 +0100

    Add home icon for Home Tab

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,10 +1,25 @@
 import { Tabs } from "expo-router";
+import Ionicons from "@expo/vector-icons/Ionicons";
 
 export default function RootLayout() {
   return (
     <Tabs>
-      <Tabs.Screen name="index" options={{ title: "Home" }} />
-      <Tabs.Screen name="about" options={{ title: "About Us" }} />
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons
+              name={focused ? "home-sharp" : "home-outline"}
+              color={color}
+              size={24}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen name="profile" options={{ title: "Profile" }} />
+      <Tabs.Screen name="map" options={{ title: "Mapa KANS" }} />
+      <Tabs.Screen name="lessons" options={{ title: "Plan zajęć" }} />
     </Tabs>
   );
 }
