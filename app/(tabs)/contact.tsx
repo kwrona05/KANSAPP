781fdb5a49717a7c14a3ec7a19f2211a4c56dd48
kwrona05 <kwrona05@gmail.com>
2025-02-09 12:12:38 +0100

    Create static contact.tsx

---
+++ app/(tabs)/contact.tsx
@@ -0,0 +1,19 @@
+import { View, Text, StyleSheet } from "react-native";
+import React from "react";
+import { FlatList } from "react-native-gesture-handler";
+
+export default function Contact() {
+  return (
+    <View>
+      <View>
+        <Text>WNHiS: 333 444 555</Text>
+      </View>
+      <View>
+        <Text>WNMiT: 444 444 555</Text>
+      </View>
+      <View>
+        <Text>Biblioteka: 555 444 555</Text>
+      </View>
+    </View>
+  );
+}
