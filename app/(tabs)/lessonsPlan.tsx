d4dc2eaad24357cb148c46537f43cb179abed4c2
kwrona05 <kwrona05@gmail.com>
2025-02-10 14:56:00 +0100

    Start Creating lessons plan

---
+++ app/(tabs)/lessonsPlan.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function Calendar() {
+  return (
+    <View>
+      <Text>Here will be calendar for students</Text>
+    </View>
+  );
+}
