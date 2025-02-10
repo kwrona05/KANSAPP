10f9598db186efe258042db030ef57e3a75901ba
kwrona05 <kwrona05@gmail.com>
2025-02-10 14:56:55 +0100

    Ad navigation elements

--- app/Navigation.tsx
+++ app/Navigation.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Contact from "./Contact";
+import Map from "./Map";
+import Announcement from "./Announcement";
+import Teachers from "./Teachers";
 
 const Stack = createNativeStackNavigator();
 
@@ -10,6 +13,9 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Contact" component={Contact} />
+        <Stack.Screen name="Map" component={Map} />
+        <Stack.Screen name="Announcement" component={Announcement} />
+        <Stack.Screen name="Teachers" component={Teachers} />
       </Stack.Navigator>
     </NavigationContainer>
   );
