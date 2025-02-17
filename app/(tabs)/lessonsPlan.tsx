ba68eb4326fd6e555ba0dd29c6db01df7902744d
kwrona05 <kwrona05@gmail.com>
2025-02-17 08:34:41 +0100

    Downloading data from json

--- app/(tabs)/lessonsPlan.tsx
+++ app/(tabs)/lessonsPlan.tsx
@@ -1,9 +1,55 @@
 import { View, Text } from "react-native";
-
+import { useEffect, useState } from "react";
+import { FlatList, GestureHandlerRootView } from "react-native-gesture-handler";
+import lessons from "../data/lessons.json";
+import { StyleSheet } from "react-native";
+import { wrap } from "module";
 export default function Calendar() {
+  const [data, setData] = useState<string[]>([]);
+
+  useEffect(() => {
+    setData(lessons.monday);
+  }, []);
+
   return (
-    <View>
-      <Text>Here will be calendar for students</Text>
-    </View>
+    <GestureHandlerRootView>
+      <View style={styles.container}>
+        <Text>Plan lekcji</Text>
+        <FlatList
+          data={data}
+          keyExtractor={(item, index) => index.toString()}
+          renderItem={({ item }) => (
+            <View style={styles.dayContainer}>
+              <Text>{item.subject}</Text>
+              <Text>
+                {item.time} - {item.category}
+              </Text>
+            </View>
+          )}
+        />
+      </View>
+    </GestureHandlerRootView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    borderWidth: 1,
+    borderRadius: 10,
+    borderColor: "#E6007E",
+  },
+  dayContainer: {
+    flex: 1,
+    flexDirection: "row",
+    gap: 10,
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: "#E6007E",
+    shadowColor: "#E6007E",
+    shadowOpacity: 0.3,
+    shadowOffset: { width: 0, height: 5 },
+    shadowRadius: 6,
+    padding: 10,
+  },
+});
