360a5c18524d291d97fc62401d4dc36b2a94135c
kwrona05 <kwrona05@gmail.com>
2025-03-10 21:54:24 +0100

    Delete useless components

--- app/Announcement.tsx
+++
@@ -1,9 +0,0 @@
-import { View, Text } from "react-native";
-
-export default function Announcement() {
-  return (
-    <View>
-      <Text>Here will be announcement</Text>
-    </View>
-  );
-}
--- app/Map.tsx
+++
@@ -1,9 +0,0 @@
-import { View, Image } from "react-native";
-
-export default function Map() {
-  return (
-    <View>
-      <Image source={require("../assets/images/Unknown.jpg")} />
-    </View>
-  );
-}
