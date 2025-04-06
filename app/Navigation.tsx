06102aa47793b3c43681d5cba7260eb8a9cbd431
kwrona05 <kwrona05@gmail.com>
2025-04-06 22:00:02 +0200

    Create navigation and initialRoute

--- app/Navigation.tsx
+++ app/Navigation.tsx
@@ -3,15 +3,19 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Teachers from "./Teachers";
 import Profile from "./Profile";
+import Home from "./(tabs)/index";
+import Login from "./Login";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="Teachers" component={Teachers} />
         <Stack.Screen name="Profile" component={Profile} />
+        <Stack.Screen name="Home" component={Home} />
       </Stack.Navigator>
     </NavigationContainer>
   );
