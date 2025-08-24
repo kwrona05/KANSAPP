e3ee840b368fdcdd7fc97d34a469acea22aac78c
kwrona05 <kwrona05@gmail.com>
2025-08-24 22:05:24 +0200

    Add new navigation screen

--- app/Navigation.tsx
+++ app/Navigation.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import Teachers from "./Teachers";
+import WeeklyEvents from "./(tabs)/calendar";
 import Profile from "./Profile";
 import Home from "./(tabs)/index";
 import Login from "./Login";
+import Teachers from "./Teachers";
 
 const Stack = createNativeStackNavigator();
 
@@ -13,9 +14,10 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Login">
         <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="Teachers" component={Teachers} />
+        <Stack.Screen name="WeeklyEvents" component={WeeklyEvents} />
         <Stack.Screen name="Profile" component={Profile} />
         <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Teachers" component={Teachers} />
       </Stack.Navigator>
     </NavigationContainer>
   );
